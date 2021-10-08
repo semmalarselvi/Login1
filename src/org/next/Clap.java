79ba2c1ad13c97402196f9ea895b13e3db187015
semmalarselvi <malarveena.2004@gmail.com>
2021-10-08 13:40:51 +0530

    Folder

---
+++ src/org/next/Clap.java
@@ -0,0 +1,14 @@
+package org.next;
+
+public class Clap {
+	public static void Login() {
+		System.out.println("Login");
+	}
+	public static void what() {
+System.out.println("hai");
+}
+public static void main(String[] args) {
+	Login();
+	what();
+}
+}
