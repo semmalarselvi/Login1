ffb21a298639eb44a102e69d5478bc8f86bab577
semmalarselvi <malarveena.2004@gmail.com>
2021-10-08 15:10:48 +0530

    add

---
+++ src/org/next/BaseClass.java
@@ -0,0 +1,12 @@
+package org.next;
+
+import java.nio.charset.MalformedInputException;
+
+public class BaseClass {
+	public static void add() {
+		System.out.println("add");
+		}
+	public static void main(String[] args) {
+		add();
+	}
+}
